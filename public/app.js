1b41b4c5f7ffb8e07a7a90c70ff8610d80eaad11
Ayush <kumarayush197@gmail.com>
2021-01-19 19:50:38 +0530

    ham-anim

--- public/app.js
+++ public/app.js
@@ -1,9 +1,15 @@
-const hamburger = document.querySelector(".ham");
+var ham = document.querySelector(".ham");
+var line1 = document.querySelector(".line-1");
+var line2 = document.querySelector(".line-2");
+var line3 = document.querySelector(".line-3");
 const navLinks = document.querySelector(".nav-links");
 const links = document.querySelectorAll(".nav-links li");
-const body = document.querySelector('body')
+const body = document.querySelector('body');
 
-hamburger.addEventListener("click", () => {
+ham.addEventListener("click", () => {
+    line1.classList.toggle("anim1");
+    line2.classList.toggle("anim2");
+    line3.classList.toggle("anim3");
     navLinks.classList.toggle("open");
     body.classList.toggle("overflow");
-});
+})
